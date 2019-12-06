124fe2ce7f76c9d36638c95ed7cd1d4150332f00
Chance Mims <chancemims1@gmail.com>
2019-12-06 17:31:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,13 @@ def long_planeteer_calls(more_planeteer_calls)# code an argument here
   more_planeteer_calls.any?{|more_element_calls| more_element_calls.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(my_snacks)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  first_cheese = my_snacks.include?(cheese_types)
+  if !first_cheese
+    return nil
+  else
+    return first_cheese
+  end
 end
