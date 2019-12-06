bbb9efd5ecf7baf8c276299752ded962507d70c5
Chance Mims <chancemims1@gmail.com>
2019-12-06 17:18:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(dwarf_list)# code an argument here
   # Your code here
-  dwarf_list.each_with_index{|my_dwarves, index| p my_dwarves}
+  dwarf_list.each_with_index{|my_dwarves, index| p "#{{index}.*}"my_dwarves}
 end
 
 def summon_captain_planet# code an argument here
